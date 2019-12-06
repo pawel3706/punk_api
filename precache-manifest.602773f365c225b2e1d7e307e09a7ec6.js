f7b984ccff73298c3d57380390cfc187b1cba641
Paweł Nowiński <pawel3706@gmail.com>
2019-12-06 11:35:42 +0100

    Updates

---
+++ precache-manifest.602773f365c225b2e1d7e307e09a7ec6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "981b35e7642ac1f0420eb37dedf66042",
+    "url": "/punk_api/index.html"
+  },
+  {
+    "revision": "f8369282bebc1b9f5615",
+    "url": "/punk_api/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "485b27fe3d78050569e6",
+    "url": "/punk_api/static/css/main.356baacb.chunk.css"
+  },
+  {
+    "revision": "f8369282bebc1b9f5615",
+    "url": "/punk_api/static/js/2.85e385df.chunk.js"
+  },
+  {
+    "revision": "485b27fe3d78050569e6",
+    "url": "/punk_api/static/js/main.d1a15bcc.chunk.js"
+  },
+  {
+    "revision": "01be5665da630d1f883e",
+    "url": "/punk_api/static/js/runtime-main.3e6c4d90.js"
+  },
+  {
+    "revision": "2b3c14e3aff33ef3be9bd91ff0526465",
+    "url": "/punk_api/static/media/keg.2b3c14e3.png"
+  }
+]);
